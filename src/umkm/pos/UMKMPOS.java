c652fb088b5067658c4b0f19534862c89ea0a404
norlaklep <fajar.kadir30@gmail.com>
2023-12-26 19:57:45 +0700

    first commit

---
+++ src/umkm/pos/UMKMPOS.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package umkm.pos;
+
+/**
+ *
+ * @author ROG
+ */
+public class UMKMPOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
